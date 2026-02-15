7542f2cffb44def959b02e52aab3751a7b1f4c57
Ben Szymanski <bszyman@icloud.com>
2026-02-15 15:44:32 -0600

    Remove duplicated tests

--- FastGooey.Tests/Controllers/AppleMobileContentControllerTests.cs
+++ FastGooey.Tests/Controllers/AppleMobileContentControllerTests.cs
@@ -51,62 +51,6 @@ public class AppleMobileContentControllerTests
         Assert.False(isValid);
         Assert.Contains(results, r => r.MemberNames.Contains("Headline"));
     }
-
-    [Fact]
-    public async Task SaveText_ReturnsEditorPanel_WhenModelStateIsInvalid()
-    {
-        var clock = new TestClock(Instant.FromUtc(2024, 1, 1, 12, 0));
-        using var dbContext = TestDbContextFactory.Create(clock);
-        var controller = new AppleMobileContentController(
-            NullLogger<AppleMobileContentController>.Instance,
-            new StubKeyValueService(),
-            dbContext);
-        controller.ControllerContext = new ControllerContext
-        {
-            HttpContext = new DefaultHttpContext()
-        };
-        controller.ModelState.AddModelError("Text", "The Text field is required.");
-
-        var result = await controller.SaveText(Guid.NewGuid(), Guid.NewGuid().ToString(), null, new TextContentFormModel());
-
-        var partial = Assert.IsType<PartialViewResult>(result);
-        Assert.Equal("~/Views/AppleMobileContent/Partials/ContentTextConfigurationPanel.cshtml", partial.ViewName);
-        Assert.Equal("#editorPanel", controller.Response.Headers["HX-Retarget"].ToString());
-    }
-    
-    [Fact]
-    public async Task SaveHeadline_ReturnsHeadlinePanelWithRetargetHeader_WhenModelStateIsInvalid()
-    {
-        using var dbContext = TestDbContextFactory.Create(new TestClock(Instant.FromUtc(2024, 1, 1, 12, 0)));
-        var controller = new AppleMobileContentController(
-            NullLogger<AppleMobileContentController>.Instance,
-            new StubKeyValueService(),
-            dbContext);
-        controller.ControllerContext = new ControllerContext
-        {
-            HttpContext = new DefaultHttpContext()
-        };
-        controller.ModelState.AddModelError("Headline", "Required");
-
-        var result = await controller.SaveHeadline(Guid.NewGuid(), Guid.NewGuid().ToString(), null, new HeadlineContentFormModel());
-
-        var partial = Assert.IsType<PartialViewResult>(result);
-        Assert.Equal("~/Views/AppleMobileContent/Partials/ContentHeadlineConfigurationPanel.cshtml", partial.ViewName);
-        Assert.Equal("#editorPanel", controller.Response.Headers["HX-Retarget"]);
-    }
-
-    [Fact]
-    public void HeadlineContentFormModel_RequiresHeadline()
-    {
-        var form = new HeadlineContentFormModel { Headline = string.Empty };
-        var context = new ValidationContext(form);
-        var results = new List<ValidationResult>();
-
-        var isValid = Validator.TryValidateObject(form, context, results, validateAllProperties: true);
-
-        Assert.False(isValid);
-        Assert.Contains(results, r => r.MemberNames.Contains("Headline"));
-    }
     
     [Fact]
     public async Task SaveText_ReturnsEditorPanel_WhenModelStateIsInvalid()
