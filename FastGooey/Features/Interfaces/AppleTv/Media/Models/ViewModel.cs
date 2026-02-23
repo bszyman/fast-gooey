46e40066bfca6e0da807d3305c564737f3c255a7
Ben Szymanski <bszyman@icloud.com>
2026-02-22 18:37:49 -0600

    Project structure cleanup: AppleTV Media

--- FastGooey/Features/Interfaces/AppleTv/Media/Models/ViewModel.cs
+++ FastGooey/Features/Interfaces/AppleTv/Media/Models/ViewModel.cs
@@ -3,6 +3,21 @@ using FastGooey.Utils;
 
 namespace FastGooey.Features.Interfaces.AppleTv.Media.Models;
 
+public class AppleTvMediaIndexViewModel
+{
+    public MediaWorkspaceViewModel? Workspace { get; set; }
+
+    public string WorkspaceId()
+    {
+        return Workspace!.ContentNode!.Workspace.PublicId.ToString();
+    }
+
+    public string InterfaceId()
+    {
+        return Workspace!.InterfaceId();
+    }
+}
+
 public class MediaWorkspaceViewModel
 {
     public GooeyInterface? ContentNode { get; set; }
@@ -18,3 +33,4 @@ public class MediaWorkspaceViewModel
         return ContentNode!.DocId.ToBase64Url();
     }
 }
+
